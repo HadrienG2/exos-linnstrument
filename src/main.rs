5796ca54196e2bfd9cb2f91fba905b7c4e33e8c0
Hadrien G. <knights_of_ni@gmx.com>
2024-05-05 14:47:14 +0200

    Fancier Unicode

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,19 @@ fn main() {
     let prog_majeur: &[&[&str]] = &[
         // Faites avec I, IV et V
         &[
-            "I I6 IV V",
+            "I I⁶ IV V",
             "I IV V",
             "I IV I V",
             "I V IV I",
-            "I V6 IV V",
-            "I I6 IV V",
+            "I V⁶ IV V",
+            "I I⁶ IV V",
         ],
         // Faites avec I, IV, V et vi
         &[
             "I IV V vi",
             "I IV vi V",
             "I V vi IV",
-            "I V6 vi IV",
+            "I V⁶ vi IV",
             "I vi IV V",
             "I vi V IV",
             "vi IV I V",
@@ -29,7 +29,7 @@ fn main() {
         &["I iii vi V", "I vi iii V"],
         // Autres progressions, pas groupées pour l'instant
         &["I IV"],
-        &["I I6 vi7 V"],
+        &["I I⁶ vi7 V"],
         &["I II IV V"],
         &["I iii IV V"],
         &["I III IV V"],
@@ -38,11 +38,11 @@ fn main() {
     let prog_dorien: &[&[&str]] = &[
         &["i ii III ii"],
         &["i III IV IV"],
-        &["i III IV bVI"],
+        &["i III IV ♭VI"],
         &["i IV i"],
         &["i v IV i"],
         &["i VII III IV"],
-        &["i VII bVI IV"],
+        &["i VII ♭VI IV"],
     ];
     let prog_phrygien: &[&[&str]] = &[
         &["i II"],
@@ -50,13 +50,13 @@ fn main() {
         &["i II III II"],
         &["i III vii II"],
         &["i iv III II"],
-        &["i vii6 II III7"],
+        &["i vii⁶ II III7"],
     ];
     let prog_lydien: &[&[&str]] = &[
         &["I I II V"],
         &["I II iii II"],
         &["I II vii iii"],
-        &["I iii II6 V"],
+        &["I iii II⁶ V"],
         &["I V iii II"],
     ];
     let prog_mixolydien: &[&[&str]] = &[
@@ -68,10 +68,10 @@ fn main() {
     let prog_mineur: &[&[&str]] = &[
         &["i III iv"],
         &["i III VII iv", "i III VII v"],
-        &["i iv iim7b5 V"],
+        &["i iv iim7♭5 V"],
         &["i iv III VI"],
         &["i iv v i", "i iv V i"],
-        &["i v VI VII", "i v6 VI VII"],
+        &["i v VI VII", "i v⁶ VI VII"],
         &["i vi ii V"],
         &["i VI III VII"],
         &["i VII VI V", "i VII VI VII"],
